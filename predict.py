6c1368dd8c28e0ca6ce05608b410b3549251fdab
sunil-linearloop <sunil@linearloop.io>
2024-09-12 09:31:23 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -33,7 +33,7 @@ class Predictor(BasePredictor):
         )
         self.diarization_model = Pipeline.from_pretrained(
             "pyannote/speaker-diarization-3.1",
-            use_auth_token="hf_token",
+            use_auth_token="HF_TOKEN",
         ).to(torch.device("cuda"))
 
     def predict(
